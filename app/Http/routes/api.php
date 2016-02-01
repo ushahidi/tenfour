b0bad879c4d474558ed8dd1524f6858ea39844e5
Jason Mule <jason.mule@gmail.com>
2016-02-01 15:08:41 +0300

    Configure codeception

--- app/Http/routes/api.php
+++ app/Http/routes/api.php
@@ -2,17 +2,32 @@
 
 use Dingo\Api\Routing\Router;
 
+$version = 'v1';
+$base = 'api/'.$version.'/';
 $api = app(Router::class);
-$api->version('v1', [
+
+$api->version($version, [
 	'namespace' => 'RollCall\Http\Controllers\Api\First',
 	'protected' => true,
-], function ($api) {
+	'middleware' => 'api.auth',
+					 'scopes' => ['user'],
+], function ($api) use ($base) {
 	// Authentication
 	//////////////////////////////////////////////////////////////////////
-	$api->post('auth', ['protected' => false, 'uses' => 'AuthController@token']);
+	// XXX: handled through oauth endpoint
+	//$api->post('auth', ['protected' => false, 'uses' => 'AuthController@token']);
 
+	// Welcome
+	/*
+	$api->get($base, ['uses' => function () {
+
+	}]);
+	*/
+	
     //Users 
     ////////////////////////////////////////////////////////////////////
-    $api->get('users', ['uses' => 'UsersController@show']);
-    $api->post('users', ['uses' => 'UsersController@create']);
+    $api->get($base.'users', ['as' => 'users.index', 'uses' => 'UsersController@show']);
+    $api->post($base.'users', ['uses' => 'UsersController@create']);
 });
+
+app('Dingo\Api\Routing\UrlGenerator')->version('v1')->route('users.index');
