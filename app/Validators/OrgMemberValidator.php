f44d45704a51211f058093e231bc7a7deb52a53d
Jason Mule <jason.mule@gmail.com>
2016-10-10 22:09:14 +0300

    Refactor org_contact validator

---
+++ app/Validators/OrgMemberValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RollCall\Validators;
+
+use RollCall\Contracts\Repositories\RollCallRepository;
+use RollCall\Contracts\Repositories\ContactRepository;
+use RollCall\Contracts\Repositories\OrganizationRepository;
+
+class OrgMemberValidator
+{
+    public function __construct(RollCallRepository $roll_call_repo, ContactRepository $contact_repo, OrganizationRepository $org_repo)
+    {
+        $this->roll_call_repo = $roll_call_repo;
+        $this->contact_repo = $contact_repo;
+        $this->org_repo = $org_repo;
+    }
+
+    public function validateContact($attr, $value, $params)
+    {
+        $roll_call_id = $params[0];
+        $contact_id = $value;
+
+        $rollcall = $this->roll_call_repo->find($roll_call_id);
+        $user = $this->contact_repo->getUser($contact_id);
+
+        return $this->org_repo->isMember($user['id'], $rollcall['organization_id']);
+    }
+}
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -76,7 +76,7 @@ return [
     'timezone'             => 'The :attribute must be a valid zone.',
     'unique'               => 'The :attribute has already been taken.',
     'url'                  => 'The :attribute format is invalid.',
-    'org_member'           => 'The contact does not belong to a member of the organization',
+    'org_contact'           => 'The contact does not belong to a member of the organization',
 
     /*
     |--------------------------------------------------------------------------
