74f29b779b61bea7ed5dcb03385e02f843b5916c
David Losada <david@tuxpiper.com>
2016-11-18 12:34:34 +0100

    support for deploying with codeship , running locally with docker-compose

--- docker/run.run.sh
+++ docker/run.run.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 set -e
 
-function sync {
-  echo "Sync files from /vols/src"
-  rsync -ar --delete-during /vols/src/ ./
-  echo "Clean uncommited files"
-  git clean -fx
-}
-
 function wait_for_mysql {
-  until nc -z mysql 3306; do
+  until nc -z ${DB_HOST} 3306; do
     >&2 echo "Mysql is unavailable - sleeping"
     sleep 1
   done
 }
 
-sync
 php -S api.rollcall.dev:80 -t public public/index.php &
 wait_for_mysql
 composer install --no-interaction
-cp .env.testing .env
+cp .env.docker.run .env
 ./artisan migrate
 
-exec "$@"
+exec $*
