2b133d4afbfa4a462c421c31efb4e77729df958b
Jason Mule <jason.mule@gmail.com>
2017-01-31 22:03:05 +0300

    Fix Nexmo API tests

--- tests/api/SMSCest.php
+++ tests/api/SMSCest.php
@@ -18,25 +18,53 @@ class SMSCest
             'message' => 'test MO from Africa\'s talking',
             'message_id' => '111111',
         ]);
-        
+
         $I->seeResponseCodeIs(200);
     }
 
-    public function receiveNexmoMOs(ApiTester $I)
+    public function receiveNexmoMOsWithoutSignature(ApiTester $I)
     {
         $endpoint = 'sms/receive/nexmo';
-        $I->wantTo('Receive MOs from Nexmo');
+        $I->wantTo('Receive MOs from Nexmo without a valid signature');
         $I->sendPost($endpoint, [
             'msisdn' => '0721674180',
             'text' => 'test MO from Nexmo',
             'messageId' => '000000FFFB0356D1',
         ]);
 
+        $I->seeResponseCodeIs(404);
+    }
+
+    public function receiveNexmoMOsWithValidSignature(ApiTester $I)
+    {
+        $secret = $I->getApplication()->config->get('rollcall.messaging.nexmo_security_secret');
+
+        // Current timestamp in UTC + 0
+        $params = [
+            'msisdn' => '0721674180',
+            'text' => 'test MO from Nexmo',
+            'messageId' => '000000FFFB0356D1',
+            'message-timestamp' => date('Y-m-d H:i:s', (time() - date('Z'))),
+        ];
+
+        ksort($params);
+
+        $params = $params + [
+            'sig' => md5('&' .urldecode(http_build_query($params)) . $secret)
+        ];
+
+        $endpoint = 'sms/receive/nexmo';
+
+        $I->wantTo('Receive MOs from Nexmo without a valid signature');
+
+        $I->sendPost($endpoint, $params);
+
         $I->seeRecord('replies', [
             'message' => 'test MO from Nexmo',
             'message_id' => '000000FFFB0356D1',
         ]);
-        
+
+
         $I->seeResponseCodeIs(200);
     }
 }
