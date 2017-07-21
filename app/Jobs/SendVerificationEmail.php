5065c1ef255e91c7e6d749843edd50f172af2fda
David McNamara <dmcnamara@ushahidi.com>
2017-07-21 11:46:22 +0100

    fix(welcome mail): "Validate" -> "Verify"

--- app/Jobs/SendVerificationEmail.php
+++ app/Jobs/SendVerificationEmail.php
@@ -32,7 +32,7 @@ class SendVerificationEmail implements ShouldQueue
     public function handle(MessageServiceFactory $message_service_factory)
     {
         $url = 'https://app.' . config('rollcall.domain') . '/organization/email/confirmation/?email='.urlencode($this->address['address']).'&token=' . urlencode($this->address['verification_token']);
-        $subject = 'Validate your RollCall email address';
+        $subject = 'Verify your RollCall email address';
 
         $message_service = $message_service_factory->make('email');
         $message_service->setView('emails.verification');
