1df018eafde7f87d614f977b3d897c5efdd948b9
David Losada <david@tuxpiper.com>
2017-12-04 16:59:12 +0100

    fix: #656 guard use of sentry if not initialised (#855)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,7 +32,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
-        if ($this->shouldReport($e)) {
+        if (app()->bound('sentry') && $this->shouldReport($e)) {
             app('sentry')->captureException($e);
         }
         parent::report($e);
