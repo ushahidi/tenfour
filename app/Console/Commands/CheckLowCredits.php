e84505be65f2642d8b6c59572b8724361144d5fb
David McNamara <dmcnamara@ushahidi.com>
2018-07-18 11:11:22 +0100

    feature: User is alerted when they need to top up more SMS credits#1247

---
+++ app/Console/Commands/CheckLowCredits.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace TenFour\Console\Commands;
+
+use Illuminate\Console\Command;
+use TenFour\Jobs\CheckLowCredits as CheckLowCreditsJob;
+
+class CheckLowCredits extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'credits:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        CheckLowCreditsJob::dispatch();
+    }
+}
