477a23abe3b1de1722271698ae3a2adc4b977607
David McNamara <dmcnamara@ushahidi.com>
2018-08-13 12:55:47 +0100

    feature: notifications API - add param to return only a person's unread notifications

--- app/Repositories/EloquentNotificationRepository.php
+++ app/Repositories/EloquentNotificationRepository.php
@@ -6,9 +6,13 @@ use TenFour\Contracts\Repositories\NotificationRepository;
 
 class EloquentNotificationRepository implements NotificationRepository
 {
-    public function all($person_id, $offset = 0, $limit = 0)
+    public function all($person_id, $offset = 0, $limit = 0, $unread = false)
     {
-        $query = User::findOrFail($person_id)->notifications();
+        if ($unread) {
+            $query = User::findOrFail($person_id)->unreadNotifications();                    
+        } else {
+            $query = User::findOrFail($person_id)->notifications();
+        }
 
         if ($limit > 0) {
             $query = $query->offset($offset)->limit($limit);
