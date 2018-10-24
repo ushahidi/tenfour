a2551ade744abb879ad8faffada3ecac7fd47544
David McNamara <dmcnamara@ushahidi.com>
2018-10-24 16:44:33 +0100

    fix: Filter sensitive values before sending to Sentry #1249

--- config/sentry.php
+++ config/sentry.php
@@ -8,4 +8,25 @@ return array(
 
     // Capture bindings on SQL queries
     'breadcrumbs.sql_bindings' => true,
+
+    'processors' => [
+        'Raven_Processor_SanitizeHttpHeadersProcessor',
+        'Raven_Processor_SanitizeDataProcessor'
+    ],
+    'processorOptions' => [
+        'Raven_Processor_SanitizeDataProcessor' => [
+            // @codingStandardsIgnoreLine
+            'fields_re' => '/(authorization|password|passwd|secret|password_confirmation|card_number|auth_pw|authToken|api_key|client_secret)/i',
+        ],
+        'Raven_Processor_SanitizeHttpHeadersProcessor' => [
+            'sanitize_http_headers' => [
+                'Authorization',
+                'Proxy-Authorization',
+                'X-Csrf-Token',
+                'X-CSRFToken',
+                'X-XSRF-TOKEN',
+                'X-Ushahidi-Signature',
+            ]
+        ]
+    ]
 );
