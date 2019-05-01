4e8298d3fdea2b142a299d93be5239ecdced00bb
Romina <rowasc@gmail.com>
2019-05-01 17:41:00 -0300

    Fix delete query for scheduled checkins

--- app/Http/Requests/CheckIn/DeleteScheduledCheckinRequest.php
+++ app/Http/Requests/CheckIn/DeleteScheduledCheckinRequest.php
@@ -5,6 +5,7 @@ namespace TenFour\Http\Requests\CheckIn;
 use Dingo\Api\Http\FormRequest;
 use TenFour\Traits\UserAccess;
 use App;
+use TenFour\Models\CheckIn;
 
 class DeleteScheduledCheckinRequest extends FormRequest
 {
@@ -18,9 +19,8 @@ class DeleteScheduledCheckinRequest extends FormRequest
     public function authorize()
     {
         // @todo should anyone really be able to update a check-in?
-        $check_in = App::make('TenFour\Contracts\Repositories\ScheduledCheckinRepository')
-                 ->find($this->route('id'));
-        if ($this->user()->isAdmin($check_in->check_ins->organization_id)) {
+        $check_in = CheckIn::query()->where('scheduled_checkin_id', '=', $this->route('id'))->first();
+        if ($this->user()->isAdmin($check_in->organization_id)) {
             return true;
         }
 
