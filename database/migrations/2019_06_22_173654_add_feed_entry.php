f1574a42070b6cbff15861e297ca25a78fa31164
Romina <rowasc@gmail.com>
2019-06-24 16:34:57 -0300

    Add entries to alerts

---
+++ database/migrations/2019_06_22_173654_add_feed_entry.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFeedEntry extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alert_feed_entry', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->unsignedBigInteger('feed_id');
+            $table->string('title');
+            $table->string('body');
+            $table->longText('metadata')->default(null); // other options ?
+            $table->foreign('feed_id')
+                ->references('id')->on('alert_feed')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('alert_feed_entry');
+    }
+}
